66a26596016dfb3bd546bc07f1414a04c494a689
zhongboai <azbazb011@gmail.com>
2023-11-21 18:01:01 +0800

    feat:change lazyvim to my own vim part 1

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,19 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+-- keybindings
+local opt = { noremap = true, silent = true }
+vim.g.mapleader = " "
+vim.keymap.set("i", "jj", "<Esc>", opt)
+vim.keymap.set("i", "kk", "<Esc>", opt)
+vim.keymap.set("n", "<Leader>h", "^", opt)
+vim.keymap.set("n", "<Leader>l", "$", opt)
+vim.keymap.set("n", "<C-l>", "<C-w>l", opt)
+vim.keymap.set("n", "<C-h>", "<C-w>h", opt)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opt)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opt)
+vim.keymap.set("n", "<Leader>v", "<C-w>v", opt)
+vim.keymap.set("n", "<Leader>s", "<C-w>s", opt)
+-- https://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
+vim.keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { noremap = true, expr = true })
+vim.keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { noremap = true, expr = true })
---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options herelocal set = vim.o
+local set = vim.o
+-- 设置 tabstop 为 4
+set.tabstop = 4
+-- 设置 softtabstop 为 4
+set.softtabstop = 4
+-- 设置 shiftwidth 为 4
+set.shiftwidth = 4
+set.number = true
+set.clipboard = "unnamed"
