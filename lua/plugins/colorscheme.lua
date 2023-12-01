472859e37ee442574f8d94f42ce6a1776f01e2cf
zhongboai <azbazb011@gmail.com>
2023-12-01 20:10:17 +0800

    colorscheme-everforest

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,39 @@
+return {
+  "neanias/everforest-nvim",
+  version = false,
+  lazy = false,
+  priority = 1000, -- make sure to load this before all the other start plugins
+  -- Optional; default configuration will be used if setup isn't called.
+  config = function()
+    require("everforest").setup({
+      -- Your config here
+    })
+  end,
+  -- Configure LazyVim to load the colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
+
+-- return {
+--   "EdenEast/nightfox.nvim",
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "nightfox",
+--     },
+--   },
+-- }
+--
 -- return {
 --   "folke/tokyonight.nvim",
 --   lazy = true,
 --   opts = { style = "moon" },
 -- }
+
 -- return {
 --   "catppuccin/nvim",
 --   lazy = true,
@@ -52,40 +83,40 @@
 --   },
 -- }
 --
-return {
-  "ellisonleao/gruvbox.nvim",
-  config = true,
-  opts = {
-    terminal_colors = true, -- add neovim terminal colors
-    undercurl = true,
-    underline = true,
-    bold = true,
-    italic = {
-      strings = true,
-      emphasis = true,
-      comments = true,
-      operators = false,
-      folds = true,
-    },
-    strikethrough = true,
-    invert_selection = false,
-    invert_signs = false,
-    invert_tabline = false,
-    invert_intend_guides = false,
-    inverse = true, -- invert background for search, diffs, statuslines and errors
-    contrast = "hard", -- can be "hard", "soft" or empty string
-    palette_overrides = {},
-    overrides = {},
-    dim_inactive = false,
-    transparent_mode = false,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
-  },
-}
+-- return {
+--   "ellisonleao/gruvbox.nvim",
+--   config = true,
+--   opts = {
+--     terminal_colors = true, -- add neovim terminal colors
+--     undercurl = true,
+--     underline = true,
+--     bold = true,
+--     italic = {
+--       strings = true,
+--       emphasis = true,
+--       comments = true,
+--       operators = false,
+--       folds = true,
+--     },
+--     strikethrough = true,
+--     invert_selection = false,
+--     invert_signs = false,
+--     invert_tabline = false,
+--     invert_intend_guides = false,
+--     inverse = true, -- invert background for search, diffs, statuslines and errors
+--     contrast = "hard", -- can be "hard", "soft" or empty string
+--     palette_overrides = {},
+--     overrides = {},
+--     dim_inactive = false,
+--     transparent_mode = false,
+--   },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   },
+-- }
 -- return {
 --   "craftzdog/solarized-osaka.nvim",
 --   lazy = false,
@@ -98,26 +129,6 @@ return {
 --     "LazyVim/LazyVim",
 --     opts = {
 --       colorscheme = "solarized-osaka",
---     },
---   },
--- }
-
--- return {
---   "neanias/everforest-nvim",
---   version = false,
---   lazy = false,
---   priority = 1000, -- make sure to load this before all the other start plugins
---   -- Optional; default configuration will be used if setup isn't called.
---   config = function()
---     require("everforest").setup({
---       -- Your config here
---     })
---   end,
---   -- Configure LazyVim to load the colorscheme
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       colorscheme = "everforest",
 --     },
 --   },
 -- }
